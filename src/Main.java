9403f3f40b04ffe36ef7d18890c4d6f9a31db856
ZhinanChen <chenzn09719@hotmail.com>
2018-11-14 23:21:42 +0800

    version3 finished

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
         System.out.println("Hello World!");
         System.out.println("This is the second version");
+        System.out.println("This is the third version");
     }
 }
